70d176b99594c1438e51c94590506dbe5d63c525
Silent-Suspect <Sascha.Mueller@rub.de>
2025-12-02 05:45:34 +0100

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,14 @@
+export const state = {
+    shifts: [],
+    activeShiftId: null,
+    timerInterval: null
+};
+
+// Konfiguration
+export const CONFIG = {
+    MIN_REST_HOURS: 10,
+    SHIFT_GAP_THRESHOLD_HOURS: 6,
+    AUTO_RESUME_THRESHOLD_MINUTES: 5,
+    // DEINE URL:
+    GATEKEEPER_URL: "https://script.google.com/macros/s/AKfycbxSjCl4LOJpjhl9MuDxOP9TLupsa7-HFHRJvL11PNxx_AXUhSYosOiLYko2XCpVHw/exec"
+};
