b89b76bb8bb52ad2f7108c5bc5905b065bb94629
Silent-Suspect <Sascha.Mueller@rub.de>
2025-12-03 21:23:52 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -11,5 +11,5 @@ export const CONFIG = {
     SHIFT_GAP_THRESHOLD_HOURS: 6,
     AUTO_RESUME_THRESHOLD_MINUTES: 5,
     // HIER DEINE NEUE GATEKEEPER URL EINTRAGEN:
-    GATEKEEPER_URL: "https://script.google.com/macros/s/AKfycbx9BEX0KZLq9YHcUubYs2q1srJ8wp-AbqClMoVkDLxneWr44RLBfcgUV171DMgZvkQ/exec"
+    GATEKEEPER_URL: "https://script.google.com/macros/s/AKfycbyL9aI1wfZ0bdQsE_TsdNUM1QVajAfgnYyZFCh_6_2GTtTW9fOHYEdtUXdK87uQKGw/exec"
 };
