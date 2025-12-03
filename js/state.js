7d4437511603b7f9b7dad72f283d2ec4cc521524
Silent-Suspect <Sascha.Mueller@rub.de>
2025-12-03 21:10:53 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,5 +1,6 @@
 export const state = {
     shifts: [],
+    deletedShifts: [], // Speicher für gelöschte Einträge
     activeShiftId: null,
     timerInterval: null
 };
@@ -9,6 +10,6 @@ export const CONFIG = {
     MIN_REST_HOURS: 10,
     SHIFT_GAP_THRESHOLD_HOURS: 6,
     AUTO_RESUME_THRESHOLD_MINUTES: 5,
-    // DEINE URL:
-    GATEKEEPER_URL: "https://script.google.com/macros/s/AKfycbx9BEX0KZLq9YHcUubYs2q1srJ8wp-AbqClMoVkDLxneWr44RLBfcgUV171DMgZvkQ/exec"
+    // HIER DEINE NEUE GATEKEEPER URL EINTRAGEN:
+    GATEKEEPER_URL: "https://script.google.com/macros/s/AKfycbyBGEprqn_IpNMqxpPuGNa4vIrw0AGwjHnabEmdHi_aeJMDQxKVI5SYe5BaYzYcv-BH/exec"
 };
