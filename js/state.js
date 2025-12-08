8b878faeba813b7a533a8d75c9894bbd91c34fc0
Silent-Suspect <Sascha.Mueller@rub.de>
2025-12-08 02:00:38 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -6,10 +6,12 @@ export const state = {
     undoStack: [] 
 };
 
+// Konfiguration
 export const CONFIG = {
     MIN_REST_HOURS: 10,
     SHIFT_GAP_THRESHOLD_HOURS: 6,
     AUTO_RESUME_THRESHOLD_MINUTES: 5,
-    AUTO_MERGE_WAIT_MINUTES: 20, // <--- NEU
+    AUTO_MERGE_WAIT_MINUTES: 20, // NEU: Schwelle für Auto-Merge
+    // HIER DEINE NEUE GATEKEEPER URL EINTRAGEN:
     GATEKEEPER_URL: "https://script.google.com/macros/s/AKfycbyL9aI1wfZ0bdQsE_TsdNUM1QVajAfgnYyZFCh_6_2GTtTW9fOHYEdtUXdK87uQKGw/exec"
 };
